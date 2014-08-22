a4576229253c904b8bc18f021a3fea940d39ac9b
Ikey Doherty <ikey.doherty@gmail.com>
2014-08-22 21:52:06 +0100

    Add Google Chrome

---
+++ network/web/browser/google-chrome-stable/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf google-chrome-stable_current_amd64.deb")
+    shelltools.system("tar xvf data.tar.lzma")
+
+def install():
+    # root owns sandbox as it is setuid
+    shelltools.system("chown root:root opt/google/chrome/chrome-sandbox")
+    # ensure setuid
+    shelltools.system("chmod 4755 opt/google/chrome/chrome-sandbox")
+    pisitools.insinto("/", "opt")
+    pisitools.insinto("/", "usr")
