4293d93f5e043d605b5f941f0e33b5da0689f769
Ikey Doherty <ikey@solus-project.com>
2015-07-25 18:57:40 +0100

    Switch to versioned deb. Make life easy

--- network/web/browser/google-chrome-stable/actions.py
+++ network/web/browser/google-chrome-stable/actions.py
@@ -7,9 +7,12 @@ from pisi.actionsapi import get, pisitools, shelltools
 NoStrip = ["/opt", "/usr"]
 IgnoreAutodep = True
 
+# Should not change.
+Suffix = "-1"
+
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf google-chrome-stable_current_amd64.deb")
+    shelltools.system("ar xf google-chrome-stable_%s%s_amd64.deb" % (get.srcVERSION(), Suffix))
     shelltools.system("tar xvf data.tar.xz")
 
 def install():
