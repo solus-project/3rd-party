81e9e23e48c7d7deb162e3af5ffc7399d16df2cd
Justin Zobel <justin@solus-project.com>
2016-01-13 17:32:28 +1030

    Update Google Chrome Unstable to 49.0.2618.8

---
+++ multimedia/music/spotify/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/usr"]
+IgnoreAutodep = True
+
+Version = get.srcVERSION()
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf spotify-client_%s*_amd64.deb" % Version)
+    shelltools.system("tar xvf data.tar.gz")
+
+def install():
+    pisitools.insinto("/", "usr")
