53fefc18be3e501f490ea5709c2c267253bccb26
Joshua Strobl <joshua@stroblindustries.com>
2016-05-15 17:39:51 +0300

    Added Sublime Text 3

---
+++ programming/sublime-text-3/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+# Should not change.
+Suffix = "-1"
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf sublime-text_build-%s_amd64.deb" % (get.srcVERSION()))
+    shelltools.system("tar xvf data.tar.lzma")
+
+def install():
+    pisitools.insinto("/", "opt")
+    pisitools.insinto("/", "usr")
