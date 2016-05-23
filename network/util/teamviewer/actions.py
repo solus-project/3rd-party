d845ad16de716f5cf9d7b50f66be84da057f570d
Justin Zobel <justin@solus-project.com>
2016-05-23 11:34:13 +0930

    Initial add of TeamViewer for testing.

---
+++ network/util/teamviewer/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt"]
+IgnoreAutodep = True
+
+def install():
+    pisitools.insinto("/etc/systemd/system/", "tv_bin/script/teamviewerd.service")
+    pisitools.dosym("/etc/systemd/system/teamviewerd.service", "/etc/systemd/system/multi-user.target.wants/teamviewerd.service")
+    pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer-teamviewer11.desktop", "/usr/share/applications/teamviewer-teamviewer11.desktop")
+    pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer.png", "/usr/share/pixmaps/teamviewer.png")
+    pisitools.insinto("/opt/teamviewer", "*")
