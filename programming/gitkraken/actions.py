a6e40cdd7e410f0551ba42c5b5967aa912737213
Joshua Strobl <joshua@stroblindustries.com>
2016-05-29 05:06:20 +0300

    Added GitKraken 1.3.0

---
+++ programming/gitkraken/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+# Should not change.
+Suffix = "-1"
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf gitkraken-amd64.deb")
+    shelltools.system("tar xvf data.tar.gz")
+    shelltools.system("sed -i 's Icon=app Icon=gitkraken ' usr/share/applications/gitkraken.desktop")
+    shelltools.system("mv usr/share/pixmaps/app.png usr/share/pixmaps/gitkraken.png")
+def install():
+    pisitools.insinto("/", "usr")
