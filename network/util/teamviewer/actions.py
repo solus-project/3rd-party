f5b5e9ca8a7b0f95ff123ab06db9a28ea3949835
Justin Zobel <justin@solus-project.com>
2016-06-26 14:23:51 +0930

    Out for public testing

--- network/util/teamviewer/actions.py
+++ network/util/teamviewer/actions.py
@@ -12,17 +12,16 @@ IgnoreAutodep = True
 def build():
     shelltools.system("pwd")
     shelltools.system("ar xf teamviewer_%s_amd64.deb" % Version)
-    shelltools.system("tar xvf data.tar.bz2")
+    shelltools.system("tar xf data.tar.bz2")
 
 def install():
-    pisitools.insinto("/opt/teamviewer", "*")
+    pisitools.insinto("/opt/", "./opt/*")
+    pisitools.insinto("/etc/systemd/system", "./opt/teamviewer/tv_bin/script/teamviewerd.service")
     
     #necessary symlinks
     pisitools.dosym("/opt/teamviewer/tv_bin/script/teamviewer", "usr/bin/teamviewer")
-    pisitools.dosym("/opt/teamviewer/logfiles", "var/log/teamviewer")
-    pisitools.dosym("/opt/teamviewer/config", "etc/teamviewer")
     pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer-teamviewer11.desktop", "/usr/share/applications/teamviewer-teamviewer11.desktop")
-    pisitools.dosym("/etc/systemd/system/teamviewerd.service", "/etc/systemd/system/multi-user.target.wants/teamviewerd.service")
+    pisitools.dosym("/etc/systemd/system/teamviewerd.service", "/etc/systemd/system/multi-user.target.wants/teamviewerd.service")    
     pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer.png", "/usr/share/pixmaps/teamviewer.png")
 
     shelltools.chmod("%s/opt/teamviewer/*" % get.installDIR(),0755)  
