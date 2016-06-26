0e00c3f86b9d6cccffac5d71919ee206eff8ea96
Justin Zobel <justin@solus-project.com>
2016-06-26 12:51:50 +0930

    More teamviewer testing.

--- network/util/teamviewer/actions.py
+++ network/util/teamviewer/actions.py
@@ -4,15 +4,19 @@
 
 from pisi.actionsapi import get, pisitools, shelltools
 
-NoStrip = ["/opt"]
+WorkDir = "."
+NoStrip = ["/opt/teamviewer/tv_bin/wine/drive_c/TeamViewer/tvwine.dll.so"]
 IgnoreAutodep = True
 
 def install():
-    pisitools.insinto("/etc/systemd/system/", "tv_bin/script/teamviewerd.service")
-    pisitools.dosym("/etc/systemd/system/teamviewerd.service", "/etc/systemd/system/multi-user.target.wants/teamviewerd.service")
-    pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer-teamviewer11.desktop", "/usr/share/applications/teamviewer-teamviewer11.desktop")
-    pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer.png", "/usr/share/pixmaps/teamviewer.png")
     pisitools.insinto("/opt/teamviewer", "*")
-    shelltools.system("chmod -R o+w %s/opt/teamviewer/logfiles" %get.installDIR())
-    shelltools.system("chmod -R o+w %s/opt/teamviewer/config" %get.installDIR())
-    shelltools.system("chmod o+w %s/opt/teamviewer" %get.installDIR())
+    
+    #necessary symlinks
+    pisitools.dosym("/opt/teamviewer/tv_bin/script/teamviewer", "usr/bin/teamviewer")
+    pisitools.dosym("/opt/teamviewer/logfiles", "var/log/teamviewer")
+    pisitools.dosym("/opt/teamviewer/config", "etc/teamviewer")
+    pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer-teamviewer11.desktop", "/usr/share/applications/teamviewer-teamviewer11.desktop")
+    pisitools.dosym("/etc/systemd/system/teamviewerd.service", "/etc/systemd/system/multi-user.target.wants/teamviewerd.service")
+    pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer.png", "/usr/share/pixmaps/teamviewer.png")
+
+    shelltools.chmod("%s/opt/teamviewer/*" % get.installDIR(),0755)  
