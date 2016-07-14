191089dd6ef48ffbd1dde962048755209094381c
Federico Damián Schonborn <federicodamians@gmail.com>
2016-07-14 11:20:32 -0300

    Make it better

--- network/im/franz/actions.py
+++ network/im/franz/actions.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
-# Created For Solus Operating System
-
-from pisi.actionsapi import get, pisitools, shelltools
+from pisi.actionsapi import pisitools, shelltools
 
 NoStrip = ["/opt", "/usr"]
 IgnoreAutodep = True
@@ -10,9 +8,9 @@ IgnoreAutodep = True
 def setup():
     shelltools.system("pwd")
     shelltools.system("tar xvf Franz-linux-x64-3.1.0.tgz")
+    shelltools.system("wget https://raw.githubusercontent.com/imprecision/franz-pkgbuild/master/franz.sh")
     shelltools.system("wget https://raw.githubusercontent.com/imprecision/franz-pkgbuild/master/franz.desktop")
     shelltools.system("wget https://raw.githubusercontent.com/imprecision/franz-pkgbuild/master/franz.png")
-    shelltools.system("wget https://raw.githubusercontent.com/imprecision/franz-pkgbuild/master/franz.sh")
     shelltools.system("install -Dm755 franz.sh usr/bin/franz")
     shelltools.system("install -Dm644 franz.desktop usr/share/applications/franz.desktop")
     shelltools.system("install -Dm644 franz.png usr/share/pixmaps/franz.png")
