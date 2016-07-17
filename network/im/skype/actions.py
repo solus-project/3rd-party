cc92320f6321733833db5b370393c7ffdfd97261
QORTEC <QORTEC@users.noreply.github.com>
2016-07-17 07:15:48 -0400

    Add Skype for Linux Alpha (#28)
    Adds the Skype for Linux Alpha to 3rd-Party.

---
+++ network/im/skype/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/usr"]
+IgnoreAutodep = True
+
+Version = get.srcVERSION()
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf skypeforlinux-64-alpha.deb")
+    shelltools.system("tar xf data.tar.xz")
+
+def install():
+    pisitools.insinto("/", "usr")
