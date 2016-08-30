f53f818a96e6aa23d5d8499cfcffcf2e073221fd
murnux <liam@lpmdesigns.com>
2016-08-30 10:42:12 -0700

    add spideroak to network/download and clean up actions.py
    Join the last 2 commits into one. Fix commit typo.

--- network/download/spideroak/actions.py
+++ network/download/spideroak/actions.py
@@ -4,20 +4,16 @@
 
 from pisi.actionsapi import get, pisitools, shelltools
 
-import shutil
-
 
 NoStrip = ["/"]
 IgnoreAutodep = True
 
 def setup():
-	#shutil.move('getbuild?platform=ubuntu&arch=x86_64', 'spideroakone.deb')
-	#print "Successfully renamed."
 	shelltools.system("mv getbuild* spideroakone.deb")
 	shelltools.system("pwd")
 	shelltools.system("ar xf spideroakone.deb")
 	shelltools.system("tar xf data.tar.gz")
-	
+
 
 def install():
     pisitools.insinto("/", "etc")
