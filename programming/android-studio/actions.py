01c7696c8759fe07248310c82086415eb41b2272
theSoenke <theSoenke@users.noreply.github.com>
2016-09-08 22:06:21 +0200

    Add Android Studio

---
+++ programming/android-studio/actions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+def setup():
+    shelltools.system("unzip android-studio-ide-143.3101438-linux.zip")
+
+def install():
+    pisitools.insinto("/opt/android-studio", "android-studio/*")
+    pisitools.dosym("/opt/android-studio/bin/studio.sh", "/usr/bin/android-studio")
