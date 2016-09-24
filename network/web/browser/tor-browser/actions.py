128c51a6c4e1983a7fa7b74bf45cc4b445854c1c
Joshua Strobl <joshua@stroblindustries.com>
2016-09-24 17:42:37 +0300

    Revert "Fix minor derp with tor-browser"
    This reverts commit 09a9b25dad9ffd2cfd48a9e486a5c551ed07ae73.

--- network/web/browser/tor-browser/actions.py
+++ network/web/browser/tor-browser/actions.py
@@ -9,7 +9,7 @@ NoStrip = ["/opt", "/usr"]
 IgnoreAutodep = True
 
 def setup():
-    shelltools.system("wget https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/tor-browser/files/tor-browser.desktop")
+    shelltools.system("wget wget https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/tor-browser/files/tor-browser.desktop")
 
 def install():
     pisitools.insinto("/opt/tor-browser/", "Browser/*")
