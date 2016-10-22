b94cdb21601794874cacc893383975bda39c31e6
Joshua Strobl <joshua@stroblindustries.com>
2016-10-22 23:38:57 +0300

    Added Bitwig Studio

---
+++ multimedia/music/bitwig-studio/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/usr"]
+IgnoreAutodep = True
+
+Version = get.srcVERSION()
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf bitwig-studio-%s.deb" % Version)
+    shelltools.system("tar xf data.tar.gz")
+
+def install():
+    pisitools.insinto("/", "opt")
+    pisitools.insinto("/", "usr")
