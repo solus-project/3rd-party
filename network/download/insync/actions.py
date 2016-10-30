a3fb8be39fd2281b38817f1ace18a73fecb9e54a
Mitchell Fossen <msfossen@gmail.com>
2016-10-30 15:11:20 -0500

    Add Nautilus integration

--- network/download/insync/actions.py
+++ network/download/insync/actions.py
@@ -2,17 +2,24 @@
 
 # Created For Solus Operating System
 
-from pisi.actionsapi import pisitools, shelltools
+from pisi.actionsapi import pisitools, shelltools, get
 
 NoStrip = ["/etc", "/usr"]
 
 
 def setup():
-    shelltools.system("ar xf insync_1.3.12.36116-trusty_amd64.deb")
+    shelltools.system("ar xf insync_{}-trusty_amd64.deb".format(get.srcVERSION()) )
     shelltools.system("tar xvf data.tar.gz")
-    shelltools.system("rmdir usr/share/icons/hicolor/64x64/emblems")
+
+    # enable status indicators the in nautilus file manager; requires the package
+    #    nautilus-python to be installed (should be installed automatically as 
+    #    a dependency from pspec.xml)
+    shelltools.system("ar xf insync-nautilus_{}-precise_all.deb".format(get.srcVERSION() ) )
+    shelltools.system("tar xvf data.tar.gz")
+
+    # a similar package exists for the caja file manager but requires python-caja
+    #    to be packaged for Solus first
 
 def install():
     pisitools.insinto("/", "etc")
     pisitools.insinto("/", "usr")
-        
