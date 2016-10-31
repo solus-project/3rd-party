7015d5cd5b1a260f7e68b19ad21262a75e42d633
Olaf Górski <olafgorski@protonmail.com>
2016-10-31 13:14:40 +0100

    renamed pycharm-community to pycharm-ce

--- programming/pycharm-ce/actions.py
+++ programming/pycharm-ce/actions.py
@@ -8,5 +8,5 @@ WorkDir = "."
 
 def install():
     shutil.rmtree("pycharm-community-2016.2.3/jre")
-    pisitools.insinto("/opt/pycharm-community", "pycharm-community-2016.2.3/*")
-    pisitools.dosym("/opt/pycharm-community/bin/pycharm.sh", "/usr/bin/pycharm-community")
+    pisitools.insinto("/opt/pycharm-ce", "pycharm-community-2016.2.3/*")
+    pisitools.dosym("/opt/pycharm-ce/bin/pycharm.sh", "/usr/bin/pycharm-ce")
