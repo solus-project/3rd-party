d9c1a939e547b38bcebb660609383558136088ed
Joshua Strobl <joshua@stroblindustries.com>
2016-11-03 23:36:37 +0200

    Fix Scrivener icon installation directory.

--- office/scrivener/actions.py
+++ office/scrivener/actions.py
@@ -13,6 +13,8 @@ def setup():
     shelltools.system("pwd")
     shelltools.system("ar xf scrivener-%s-amd64.deb" % Version)
     shelltools.system("tar xf data.tar.gz")
+    shelltools.system("mkdir usr/share/icons/hicolor")
+    pisitools.move("usr/share/icons/*x*", "usr/share/icons/hicolor/")
 
 def install():
     pisitools.insinto("/", "usr")
