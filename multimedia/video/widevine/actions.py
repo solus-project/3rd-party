cf3a3f11e188829aceadf40178fb45b33d441e06
Federico Damián Schonborn <federico.d.schonborn@protonmail.com>
2016-12-14 22:56:38 -0300

    Initial inclusion of the Widevine DRM Plugin in Third Party
    Signed-off-by: Federico Damián Schonborn <federico.d.schonborn@protonmail.com>

---
+++ multimedia/video/widevine/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+# Should not change.
+Suffix = "-1"
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf google-chrome-stable_%s%s_amd64.deb" % (get.srcVERSION(), Suffix))
+    shelltools.system("tar xvf data.tar.xz")
+
+def install():
+    pisitools.insinto("/opt/google/chrome/libwidevinecdm.so", "opt/google/chrome/libwidevinecdm.so")
