f9a1c5aece3a2ab85205dc822bc01c9da3fc4bd6
Niklas Heer <niklas.heer@gmail.com>
2016-12-24 03:10:59 +0100

    updates Phpstorm to 2016.3.2

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -7,6 +7,6 @@ WorkDir = "."
 
 
 def install():
-    shutil.rmtree("PhpStorm-162.1889.1/jre")
-    pisitools.insinto("/opt/phpstorm", "PhpStorm-162.1889.1/*")
+    shutil.rmtree("PhpStorm-163.10504.2/jre")
+    pisitools.insinto("/opt/phpstorm", "PhpStorm-163.10504.2/*")
     pisitools.dosym("/opt/phpstorm/bin/phpstorm.sh", "/usr/bin/phpstorm")
