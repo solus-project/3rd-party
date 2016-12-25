c54c5a6ebd0acaaac2a31d82853e3c0d9fea3aa7
Niklas Heer <niklas.heer@gmail.com>
2016-12-25 01:12:33 +0100

    makes CLion more consistent with other packages

--- programming/clion/actions.py
+++ programming/clion/actions.py
@@ -4,9 +4,9 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-
+Version =  get.srcVERSION()
 
 def install():
-    shutil.rmtree("clion-2016.3.1/jre")
-    pisitools.insinto("/opt/clion", "clion-2016.3.1/*")
+    shutil.rmtree("clion-%s/jre" % Version)
+    pisitools.insinto("/opt/clion", "clion-%s/*" % Version)
     pisitools.dosym("/opt/clion/bin/clion.sh", "/usr/bin/clion")
