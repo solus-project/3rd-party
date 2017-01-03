ca46a142caaf30fa6b8c67c5a2e2aa85019693ef
Joshua Strobl <joshua@stroblindustries.com>
2017-01-03 23:07:28 +0200

    Update Mendeley Desktop to 1.17.6

--- office/mendeleydesktop/actions.py
+++ office/mendeleydesktop/actions.py
@@ -13,8 +13,8 @@ def setup():
     shelltools.system("tar xf mendeleydesktop-%s-linux-x86_64.tar.bz2" % Version)
 
 def install():
-    pisitools.insinto("/opt/mendeleydesktop/", "mendeleydesktop-1.17-linux-x86_64/bin")
-    pisitools.insinto("/opt/mendeleydesktop/", "mendeleydesktop-1.17-linux-x86_64/lib")
-    pisitools.insinto("/usr", "mendeleydesktop-1.17-linux-x86_64/share")
+    pisitools.insinto("/opt/mendeleydesktop/", "mendeleydesktop-%s-linux-x86_64/bin" % Version)
+    pisitools.insinto("/opt/mendeleydesktop/", "mendeleydesktop-%s-linux-x86_64/lib" % Version)
+    pisitools.insinto("/usr", "mendeleydesktop-%s-linux-x86_64/share" % Version)
     pisitools.removeDir("/usr/share/doc")
     pisitools.dosym("/opt/mendeleydesktop/bin/mendeleydesktop", "/usr/bin/mendeleydesktop")
