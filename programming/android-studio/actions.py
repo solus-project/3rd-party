39ab257a33e2935e12b61915cec4ef129c97ed82
Sönke Behrendt <thesoenke@outlook.com>
2017-03-03 18:55:49 +0100

    Update Android Studio to 2.3.0.8

--- programming/android-studio/actions.py
+++ programming/android-studio/actions.py
@@ -3,7 +3,7 @@
 from pisi.actionsapi import get, pisitools, shelltools
 
 def setup():
-    shelltools.system("unzip android-studio-ide-145.3537739-linux.zip")
+    shelltools.system("unzip android-studio-ide-162.3764568-linux.zip")
 
 def install():
     pisitools.insinto("/opt/android-studio", "android-studio/*")
