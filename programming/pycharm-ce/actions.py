d711970c5781b971d8f957698d8e92b7136bc57d
Bryan T. Meyers <bmeyers@datadrake.com>
2017-04-30 12:31:55 -0400

    Updated to 2017.1.2 and fixed .desktop file

--- programming/pycharm-ce/actions.py
+++ programming/pycharm-ce/actions.py
@@ -7,6 +7,6 @@ WorkDir = "."
 Version =  get.srcVERSION()
 
 def install():
-    shutil.rmtree("pycharm-community-%s/jre" % Version)
+    shutil.rmtree("pycharm-community-%s/jre64" % Version)
     pisitools.insinto("/opt/pycharm-ce", "pycharm-community-%s/*" % Version)
     pisitools.dosym("/opt/pycharm-ce/bin/pycharm.sh", "/usr/bin/pycharm-ce")
