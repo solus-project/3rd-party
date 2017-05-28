c3ebe1f3347a4b442837cdb0ad489bde1e5e9d00
Ikey Doherty <ikey@solus-project.com>
2017-05-28 18:02:26 +0100

    mendeley: Fix start up issues per T3651 in Solus
    This change fixes the broken layout to ensure that Mendeley Desktop
    correctly functions *after* logging in.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- office/mendeleydesktop/actions.py
+++ office/mendeleydesktop/actions.py
@@ -13,8 +13,14 @@ def setup():
     shelltools.system("tar xf mendeleydesktop-%s-linux-x86_64.tar.bz2" % Version)
 
 def install():
-    pisitools.insinto("/opt/mendeleydesktop/", "mendeleydesktop-%s-linux-x86_64/bin" % Version)
-    pisitools.insinto("/opt/mendeleydesktop/", "mendeleydesktop-%s-linux-x86_64/lib" % Version)
-    pisitools.insinto("/usr", "mendeleydesktop-%s-linux-x86_64/share" % Version)
-    pisitools.removeDir("/usr/share/doc")
-    pisitools.dosym("/opt/mendeleydesktop/bin/mendeleydesktop", "/usr/bin/mendeleydesktop")
+    for component in ["bin", "lib", "share"]:
+        pisitools.insinto("/opt/mendeley/", "mendeleydesktop-%s-linux-x86_64/%s" % (Version, component))
+
+    pisitools.dodir("/usr/share/applications")
+    pisitools.dosym("../../opt/mendeley/bin/mendeleydesktop", "/usr/bin/mendeleydesktop")
+    pisitools.dosym("../../opt/mendeley/share/applications/mendeleydesktop.desktop", "/usr/share/applications/mendeleydesktop.desktop")
+
+    for icon_size in ["16", "22", "32", "48", "64", "128"]:
+        pisitools.dodir("/usr/share/icons/hicolor/%sx%s" % (icon_size, icon_size))
+        pisitools.dosym("../../../../opt/mendeley/share/icons/hicolor/%sx%s/apps/mendeleydesktop.png" % (icon_size, icon_size),
+                        "/usr/share/icons/hicolor/%sx%s/apps/mendeleydesktop.png" % (icon_size, icon_size))
