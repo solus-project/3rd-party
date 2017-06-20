a0606fd46313d65d68e47512d19ae1175782d6bf
Jérôme Groven <jerome.groven@gmail.com>
2017-06-20 21:18:43 +0200

    Update PHPStorm to 2017.1.4

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -7,6 +7,6 @@ WorkDir = "."
 
 
 def install():
-    shutil.rmtree("PhpStorm-171.4424.11/jre64")
-    pisitools.insinto("/opt/phpstorm", "PhpStorm-171.4424.11/*")
+    shutil.rmtree("PhpStorm-171.4694.2/jre64")
+    pisitools.insinto("/opt/phpstorm", "PhpStorm-171.4694.2/*")
     pisitools.dosym("/opt/phpstorm/bin/phpstorm.sh", "/usr/bin/phpstorm")
