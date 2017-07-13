70fc7a5ed851c74dd07bad65638268bcdb9cd436
Peter O'Connor <peter@solus-project.com>
2017-07-13 12:23:32 +1000

    Update Insync to 1.3.18.36169
    Signed-off-by: Peter O'Connor <peter@solus-project.com>

--- network/download/insync/actions.py
+++ network/download/insync/actions.py
@@ -7,15 +7,14 @@ from pisi.actionsapi import pisitools, shelltools, get
 NoStrip = ["/etc", "/usr"]
 KeepSpecial = ["python"]
 
-
 def setup():
-    shelltools.system("ar xf insync_{}-trusty_amd64.deb".format(get.srcVERSION() ) )
+    shelltools.system("ar xf insync_{}-*_amd64.deb".format(get.srcVERSION() ) )
     shelltools.system("tar xvf data.tar.gz")
 
     # enable status indicators the in nautilus file manager; requires the package
     #    nautilus-python to be installed (should be installed automatically as
     #    a dependency from pspec.xml)
-    shelltools.system("ar xf insync-nautilus_{}-precise_all.deb".format(get.srcVERSION() ) )
+    shelltools.system("ar xf insync-nautilus*_all.deb")
     shelltools.system("tar xvf data.tar.gz")
 
     # a similar package exists for the caja file manager but requires python-caja
