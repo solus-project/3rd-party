ae7085e6e4423d25722f4dfcd8d05957b968e879
Bryan T. Meyers <bmeyers@datadrake.com>
2017-07-27 07:59:49 -0400

    Updated CLion to 2017.2

--- programming/clion/actions.py
+++ programming/clion/actions.py
@@ -7,6 +7,6 @@ WorkDir = "."
 Version =  get.srcVERSION()
 
 def install():
-    shutil.rmtree("clion-%s/jre" % Version)
+    shutil.rmtree("clion-%s/jre64" % Version)
     pisitools.insinto("/opt/clion", "clion-%s/*" % Version)
     pisitools.dosym("/opt/clion/bin/clion.sh", "/usr/bin/clion")
