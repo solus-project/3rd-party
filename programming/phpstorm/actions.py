376ef0bcd0e0456f4297d8e6a2e9588b16e4e2ba
Bryan T. Meyers <bmeyers@datadrake.com>
2017-07-27 08:14:03 -0400

    Updated PhpStorm to 2017.2

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -7,6 +7,6 @@ WorkDir = "."
 
 
 def install():
-    shutil.rmtree("PhpStorm-171.4694.2/jre64")
-    pisitools.insinto("/opt/phpstorm", "PhpStorm-171.4694.2/*")
+    shutil.rmtree("PhpStorm-172.3317.83/jre64")
+    pisitools.insinto("/opt/phpstorm", "PhpStorm-172.3317.83/*")
     pisitools.dosym("/opt/phpstorm/bin/phpstorm.sh", "/usr/bin/phpstorm")
