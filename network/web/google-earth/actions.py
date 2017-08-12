32e34722aab5653d3d10102d38ea6166f75f2086
Peter O'Connor <peter@solus-project.com>
2017-08-12 13:23:40 +1000

    Update Google Earth to 7.3.0.3830, now the Pro version
    Earth "Free" and Pro have now been combined into a single product, Earth Pro.
    
    Fixes #190
    
    Signed-off-by: Peter O'Connor <peter@solus-project.com>

--- network/web/google-earth/actions.py
+++ network/web/google-earth/actions.py
@@ -8,12 +8,12 @@ NoStrip = ["/opt", "/usr"]
 IgnoreAutodep = True
 
 def setup():
-    shelltools.system("ar xf google-earth-stable_%s-r0_amd64.deb" % get.srcVERSION())
+    shelltools.system("ar xf google-earth-pro-stable_%s-r0_amd64.deb" % get.srcVERSION())
     shelltools.system("tar xvf data.tar.xz")
-    shelltools.system("mv opt/google/earth/free/google-earth.desktop .")
+    shelltools.system("mv opt/google/earth/pro/google-earth-pro.desktop .")
 
 def install():
     pisitools.insinto("/", "opt")
     pisitools.insinto("/usr", "usr/bin")
-    pisitools.insinto("/usr/share/applications", "google-earth.desktop")
+    pisitools.insinto("/usr/share/applications", "google-earth-pro.desktop")
     pisitools.dosym("ld-linux-x86-64.so.2", "/lib/ld-lsb-x86-64.so.3")
