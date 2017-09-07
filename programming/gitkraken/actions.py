b5e56c1a7f38af5747f0164a6d8b3bf5e9307bc1
Joshua Strobl <joshua@stroblindustries.com>
2017-09-07 20:48:21 +0300

    Fix GitKraken tar extraction.

--- programming/gitkraken/actions.py
+++ programming/gitkraken/actions.py
@@ -13,7 +13,7 @@ Suffix = "-1"
 def setup():
     shelltools.system("pwd")
     shelltools.system("ar xf gitkraken-amd64.deb")
-    shelltools.system("tar xvf data.tar.gz")
+    shelltools.system("tar xJvf data.tar.xz")
     shelltools.system("sed -i 's Icon=app Icon=gitkraken ' usr/share/applications/gitkraken.desktop")
     shelltools.system("mv usr/share/pixmaps/app.png usr/share/pixmaps/gitkraken.png")
 def install():
