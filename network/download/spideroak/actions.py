0db7ed665ee28fad1e25f48ee49d1f185fde545b
Eric Pfeiffer <computerfreak@computerfr33k.com>
2017-09-19 15:01:18 -0500

    fix code formatting

--- network/download/spideroak/actions.py
+++ network/download/spideroak/actions.py
@@ -8,10 +8,11 @@ from pisi.actionsapi import get, pisitools, shelltools
 NoStrip = ["/"]
 IgnoreAutodep = True
 
+
 def setup():
-	shelltools.system("ar xf SpiderOakONE_*.deb")
-	shelltools.system("tar xf data.tar.gz")
-	shelltools.system("rm -r etc/apt")
+    shelltools.system("ar xf SpiderOakONE_*.deb")
+    shelltools.system("tar xf data.tar.gz")
+    shelltools.system("rm -r etc/apt")
 
 
 def install():
