0fe5e50b49222328a12eeea83813761f356b2f82
Unknown <liberodark@gmail.com>
2017-10-03 20:08:08 +0200

    fix

--- desktop/tusk/actions.py
+++ desktop/tusk/actions.py
@@ -11,7 +11,7 @@ Version = get.srcVERSION()
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf tusk-%s~*amd64.deb" % Version)
+    shelltools.system("ar xf tusk-%s~*-linux-amd64.deb" % Version)
     shelltools.system("tar xf data.tar.xz")
 
 def install():
