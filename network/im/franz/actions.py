bfe56d576d412e6a26d939c05b7900741d236bc8
ahahn94 <ahahn94@outlook.com>
2017-12-11 14:10:10 +0100

    Update Franz

--- network/im/franz/actions.py
+++ network/im/franz/actions.py
@@ -9,15 +9,10 @@ IgnoreAutodep = True
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("mkdir franz")
-    shelltools.system("tar xvf Franz-linux-x64-%s.tgz -C franz" % (get.srcVERSION()))
-    shelltools.system("wget https://raw.githubusercontent.com/meetfranz/franz-pkgbuild/master/franz.sh")
-    shelltools.system("wget https://raw.githubusercontent.com/meetfranz/franz-pkgbuild/master/franz.desktop")
-    shelltools.system("wget https://raw.githubusercontent.com/meetfranz/franz-pkgbuild/master/franz.png")
-    shelltools.system("install -Dm755 franz.sh usr/bin/franz")
-    shelltools.system("install -Dm644 franz.desktop usr/share/applications/franz.desktop")
-    shelltools.system("install -Dm644 franz.png usr/share/pixmaps/franz.png")
+    shelltools.system("ar xf franz_5.0.0-beta.14_amd64.deb")
+    shelltools.system("tar xf data.tar.xz")
 
 def install():
     pisitools.insinto("/", "usr")
-    pisitools.insinto("/opt/", "franz")
+    pisitools.insinto("/", "opt")
+    pisitools.dosym("/opt/Franz/franz", "/usr/bin/franz")
