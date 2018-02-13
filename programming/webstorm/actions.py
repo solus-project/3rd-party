111cf1075eab582269c428946685b14d1dd307a9
André Hahn <ahahn94@outlook.com>
2018-02-13 14:13:23 +0100

    Update the Jetbrains IDEs to the most recent versions (#297)
    * Update CLion 2017.3.3
    
    Update CLion to version 2017.3.3 from January 31, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>
    
    * Update DataGrip 2017.3.4
    
    Update DataGrip to version 2017.3.4 from January 17, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>
    
    * Update Idea 2017.3.4
    
    Update Idea to version 2017.3.4 from January 30, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>
    
    * Update PhpStorm 2017.3.4
    
    Update PhpStorm to version 2017.3.4 from January 30, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>
    
    * Update PyCharm 2017.3.3
    
    Update PyCharm to version 2017.3.3 from January 18, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>
    
    * Update PyCharm-CE 2017.3.3
    
    Update PyCharm-CE to version 2017.3.3 from January 18, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>
    
    * Update RubyMine 2017.3.2
    
    Update RubyMine to version 2017.3.2 from January 18, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>
    
    * Update WebStorm 2017.3.4
    
    Update WebStorm to version 2017.3.4 from January 31, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>
    
    * Update Rider 2017.3.1
    
    Update Rider to version 2017.3.1 from February 6, 2018
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/webstorm/actions.py
+++ programming/webstorm/actions.py
@@ -7,6 +7,6 @@ WorkDir = "."
 
 
 def install():
-    shutil.rmtree("WebStorm-173.3942.31/jre64")
-    pisitools.insinto("/opt/webstorm", "WebStorm-173.3942.31/*")
+    shutil.rmtree("WebStorm-173.4548.30/jre64")
+    pisitools.insinto("/opt/webstorm", "WebStorm-173.4548.30/*")
     pisitools.dosym("/opt/webstorm/bin/webstorm.sh", "/usr/bin/webstorm")
