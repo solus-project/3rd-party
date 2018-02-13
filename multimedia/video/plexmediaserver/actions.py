efa773f4a988b7215367b340e472e87ea7409fc8
Ikey Doherty <ikey@solus-project.com>
2018-02-13 22:27:04 +0000

    I hate third party.
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- multimedia/video/plexmediaserver/actions.py
+++ multimedia/video/plexmediaserver/actions.py
@@ -12,17 +12,7 @@ def setup():
     shelltools.system("ar xf plexmediaserver_%s-*_amd64.deb" % get.srcVERSION())
     shelltools.system("tar xvf data.tar.*")
     shelltools.system("sed -i 's/x-www-browser/xdg-open/' usr/share/applications/plexmediamanager.desktop")
-    shelltools.system("curl -o plexmediaserver.conf https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/plexmediaserver/files/plexmediaserver.conf")
-
-    shelltools.system("curl -o plexmediaserver.tmpfile https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/plexmediaserver/files/plexmediaserver.tmpfile")
-    shelltools.system("mkdir tmp")
-    shelltools.system("mv plexmediaserver.tmpfile tmp/plexmediaserver.conf")
-
-    shelltools.system("curl -o plexmediaserver.service https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/plexmediaserver/files/plexmediaserver.service")
 
 def install():
     pisitools.insinto("/opt", "usr/lib/plexmediaserver")
-    pisitools.insinto("/usr/lib64/sysusers.d", "plexmediaserver.conf")
-    pisitools.insinto("/usr/lib64/tmpfiles.d", "tmp/plexmediaserver.conf")
-    pisitools.insinto("/usr/lib64/systemd/system", "plexmediaserver.service")
     pisitools.insinto("/usr", "usr/share")
