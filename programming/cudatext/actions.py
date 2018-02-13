eb066f850be2c15af4d1c5afb5fbab5adf365c5a
liberodark <liberodark@gmail.com>
2018-02-13 11:53:22 +0100

    Update actions.py

--- programming/cudatext/actions.py
+++ programming/cudatext/actions.py
@@ -4,7 +4,7 @@
 
 from pisi.actionsapi import get, pisitools, shelltools
 
-NoStrip = ["/opt", "/usr"]
+NoStrip = ["/usr"]
 IgnoreAutodep = True
 
 # Should not change.
@@ -12,7 +12,7 @@ Suffix = "-1"
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf cudatext_1.40.0.0-1_gtk2_amd64.deb" % (get.srcVERSION()))
+    shelltools.system("ar xf cudatext_1.40.0.0-1_gtk2_amd64.deb")
     shelltools.system("tar xvf data.tar.xz")
 
 def install():
