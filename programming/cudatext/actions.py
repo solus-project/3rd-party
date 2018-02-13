f305c1a3c98dae633e94abea1b96b82d65a79c38
liberodark <liberodark@gmail.com>
2018-02-13 11:54:03 +0100

    Update actions.py

--- programming/cudatext/actions.py
+++ programming/cudatext/actions.py
@@ -11,7 +11,6 @@ IgnoreAutodep = True
 Suffix = "-1"
 
 def setup():
-    shelltools.system("pwd")
     shelltools.system("ar xf cudatext_1.40.0.0-1_gtk2_amd64.deb")
     shelltools.system("tar xvf data.tar.xz")
 
