8d723cb878455d7c8ab0bd1c34368dc2ea18527e
ThanosApostolou <thanosapostolou@outlook.com>
2018-04-13 18:09:50 +0300

    update Google Earth to 7.3.1.4507, add launcher icons

--- network/web/google-earth/actions.py
+++ network/web/google-earth/actions.py
@@ -11,9 +11,13 @@ def setup():
     shelltools.system("ar xf google-earth-pro-stable_%s-r0_amd64.deb" % get.srcVERSION())
     shelltools.system("tar xvf data.tar.xz")
     shelltools.system("mv opt/google/earth/pro/google-earth-pro.desktop .")
+    
 
 def install():
     pisitools.insinto("/", "opt")
     pisitools.insinto("/usr", "usr/bin")
     pisitools.insinto("/usr/share/applications", "google-earth-pro.desktop")
     pisitools.dosym("ld-linux-x86-64.so.2", "/lib/ld-lsb-x86-64.so.3")
+    
+    for i in ["16", "22", "24", "32", "48", "64", "128", "256"]:
+        pisitools.insinto("/usr/share/icons/hicolor/%sx%s/apps" % (i,i), "opt/google/earth/pro/product_logo_%s.png" % i, "google-earth-pro.png")
