b5cbfb7ed3b458c8026edb09e8fa86cf86e09f76
ahahn94 <ahahn94@outlook.com>
2018-06-15 13:56:59 +0200

    Update PhpStorm 2018.1.6
    Update PhpStorm to version 2018.1.6 from June 15, 2018.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "181.5281.19"
+Build = "181.5281.35"
 
 def install():
     shutil.rmtree("PhpStorm-%s/jre64" % Build)
