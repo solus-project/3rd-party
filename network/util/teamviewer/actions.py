82f0f9f817931facb3dc260b1dbe6bf369ce428a
Staudey <Staudi.Kaos@gmail.com>
2018-08-06 04:34:11 +0200

    Update Teamviewer to version 13.1.8286 (#412)
    * try update to 64 bit teamviewer 13x
    
    * try fixing deb reference (amd64 not i386)
    
    * trial-and-error mode fully engaged. xz, not bz2.
    
    * add dependency on gtk2-engine-murrine to solve warning
    
    * fix syms. weird. as if teamviewer12 was broken.
    
    * add deps from deb. too many, will cut later.
    
    * fix sym args
    
    * drop the symlinks. create dirs.
    
    * educated guess that I need this.
    
    * last commit fully working. now guess-cutting deps.
    
    Uninstalling gives warning for failing to remove <Path fileType="log">/var/log/teamviewer13</Path>
    I arbitrarily picked filetype 'log'. It may be invalid.
    
    * leave 2 original deps
    
    * verify all added deps. remove first.
    
                <Dependency>qt5-quickcontrols</Dependency>
                <Dependency>qt5-quickcontrols2</Dependency>
                <Dependency>qt5-connectivity</Dependency>
                <Dependency>qt5-translations</Dependency>
                <Dependency>qt5-xmlpatterns</Dependency>
    
    * add quickcontrols
    
    * add connectivity
    
    * probably not needed
    
    * try starting service as part on the installation
    
    * starting service does not work here
    
    * Update Teamviewer to version 13.1.8286

--- network/util/teamviewer/actions.py
+++ network/util/teamviewer/actions.py
@@ -11,18 +11,23 @@ IgnoreAutodep = True
 
 def build():
     shelltools.system("pwd")
-    shelltools.system("ar xf teamviewer_%s_i386.deb" % Version)
-    shelltools.system("tar xf data.tar.bz2")
+    shelltools.system("ar xf teamviewer_%s_amd64.deb" % Version)
+    shelltools.system("tar xf data.tar.xz")
 
 def install():
     pisitools.insinto("/opt/", "./opt/*")
     pisitools.insinto("/etc/systemd/system", "./opt/teamviewer/tv_bin/script/teamviewerd.service")
     
     #necessary symlinks
-    pisitools.dosym("/opt/teamviewer/tv_bin/script/teamviewer", "usr/bin/teamviewer")
-    pisitools.dosym("/opt/teamviewer/tv_bin/desktop/com.teamviewer.TeamViewer.desktop", "/usr/share/applications/teamviewer-teamviewer12.desktop")
+    pisitools.dosym("/opt/teamviewer/tv_bin/script/teamviewer", "/usr/bin/teamviewer")
+    pisitools.dosym("/opt/teamviewer/tv_bin/desktop/com.teamviewer.TeamViewer.desktop", "/usr/share/applications/teamviewer-teamviewer.desktop")
     pisitools.dosym("/etc/systemd/system/teamviewerd.service", "/etc/systemd/system/multi-user.target.wants/teamviewerd.service")    
     pisitools.dosym("/opt/teamviewer/tv_bin/desktop/teamviewer.png", "/usr/share/pixmaps/teamviewer.png")
 
+    pisitools.dodir("/etc/teamviewer")
+    pisitools.dodir("/var/log/teamviewer13")
+
     shelltools.chmod("%s/opt/teamviewer/doc/*" % get.installDIR(),0755)
     shelltools.chmod("%s/opt/teamviewer/tv_bin/*" % get.installDIR(),0755)
+    
+    # shelltools.system("systemctl start teamviewerd.service")
