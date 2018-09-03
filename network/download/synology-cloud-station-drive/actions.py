b7e094eb545a9257ae5fc7c7bf68eb6856c261fc
Staudey <Staudi.Kaos@gmail.com>
2018-09-03 20:24:31 +0200

    Update archive name

--- network/download/synology-cloud-station-drive/actions.py
+++ network/download/synology-cloud-station-drive/actions.py
@@ -9,7 +9,7 @@ NoStrip = ["/"]
 IgnoreAutodep = True
 
 def setup():
-    shelltools.system("ar xf synology-cloud-station-drive-4415.x86_64.deb")
+    shelltools.system("ar xf synology-cloud-station-drive-4421.x86_64.deb")
     shelltools.system("tar xf data.tar.gz")
 
 def install():
