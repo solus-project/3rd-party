9a48f5935165847753c66e3f6aa2dbd7e721870c
James Lee <jamesl33info@gmail.com>
2018-09-20 10:07:50 +0100

    Initial packaging of sublime-merge at stable version 1055

---
+++ programming/sublime-merge/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+# Should not change.
+Suffix = "-1"
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf sublime-merge_build-%s_amd64.deb" % (get.srcVERSION()))
+    shelltools.system("tar xvf data.tar.xz")
+
+def install():
+    pisitools.insinto("/", "opt")
+    pisitools.insinto("/", "usr")
