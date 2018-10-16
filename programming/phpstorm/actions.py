6d8ab6e34ac2a273f338253ffc0555db0f5deb69
André Hahn <ahahn94@outlook.com>
2018-10-16 16:47:34 +0200

    Update PhpStorm 2018.2.5 (#502)
    Update PhpStorm to version 2018.2.5 from October 11, 2018.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/phpstorm/actions.py
+++ programming/phpstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "182.4505.42"
+Build = "182.4892.16"
 
 def install():
     shutil.rmtree("PhpStorm-%s/jre64" % Build)
