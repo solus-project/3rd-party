28c91288b7163e74148c918c585ba1376065e5b8
Thomas Staudinger <Staudi.Kaos@gmail.com>
2018-11-05 23:19:55 +0100

    Prevent WPS Office from creating own menu folder
    We don't have a separate folder for LibreOffice either, and some people seem to be confused by the extra entry, see https://plus.google.com/u/0/106168304605552025334/posts/Sdp4VRCJnFR

--- office/wps-office/actions.py
+++ office/wps-office/actions.py
@@ -13,8 +13,10 @@ def setup():
     shelltools.system("pwd")
     shelltools.system("ar xf wps-office_%s_amd64.deb" % Version)
     shelltools.system("tar xf data.tar.xz")
+    #Don't install separate menu folder entry:
+    shelltools.system("rm -r etc")
 
 def install():
     pisitools.insinto("/", "usr")
     pisitools.insinto("/", "opt")
-    pisitools.insinto("/", "etc")
+    #pisitools.insinto("/", "etc")
