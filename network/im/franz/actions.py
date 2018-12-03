c68d6c9e814cfe51db1046842b80efcbed404e5b
Philipp Trulson <philipp@trulson.de>
2018-12-03 14:23:48 +0100

    Update Franz to 5.0.0 Beta 19

--- network/im/franz/actions.py
+++ network/im/franz/actions.py
@@ -9,7 +9,7 @@ IgnoreAutodep = True
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf franz_5.0.0-beta.18_amd64.deb")
+    shelltools.system("ar xf franz_5.0.0-beta.19_amd64.deb")
     shelltools.system("tar xf data.tar.xz")
 
 def install():
