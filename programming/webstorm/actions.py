f44d2fba2d147de45fea3d033795cd509a0ed825
ahahn94 <ahahn94@outlook.com>
2019-03-02 12:41:51 +0100

    Update WebStorm 2018.3.5
    Update WebStorm to version 2018.3.5 from February 27, 2019.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- programming/webstorm/actions.py
+++ programming/webstorm/actions.py
@@ -4,7 +4,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 import shutil
 
 WorkDir = "."
-Build = "183.5429.34"
+Build = "183.5912.23"
 
 def install():
     shutil.rmtree("WebStorm-%s/jre64" % Build)
