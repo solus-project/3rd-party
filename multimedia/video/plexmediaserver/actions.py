f6f92fc031fb3f379f56618003eaccfeda294e0e
Thomas Staudinger <Staudi.Kaos@gmail.com>
2019-03-14 18:49:33 +0100

    Update Plex Media Server to 1.15.1.791

--- multimedia/video/plexmediaserver/actions.py
+++ multimedia/video/plexmediaserver/actions.py
@@ -11,7 +11,7 @@ IgnoreAutodep = True
 def setup():
     shelltools.system("ar xf plexmediaserver_%s-*_amd64.deb" % get.srcVERSION())
     shelltools.system("tar xvf data.tar.*")
-    shelltools.system("sed -i 's/x-www-browser/xdg-open/' usr/share/applications/plexmediamanager.desktop")
+    shelltools.system("sed -i 's/x-www-browser/xdg-open/' usr/share/applications/plexmediaserver.desktop")
 
 def install():
     pisitools.insinto("/opt", "usr/lib/plexmediaserver")
